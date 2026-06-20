3f359d8e7029a1c442a6954cd34f361e1c636870
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2026-06-20 11:25:48 +0200

    Refactoring: Implementing Posts service.

---
+++ CircleApp.Data/Services/IPostsService.cs
@@ -0,0 +1,22 @@
+﻿
+using CircleApp.Data.Persistence.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace CircleApp.Data.Services
+{
+    public interface IPostsService
+    {
+        Task<List<Post>> GetPostsForUserAsync(int userId);
+        Task<Post> GetPostByIdAsync(int postId);
+        Task CreatePostAsync(Post post, IFormFile Image);
+        Task DeletePostAsync(int postId);
+
+        Task AddPostCommentAsync(Comment comment);
+        Task RemovePostCommentAsync(int commentId);
+
+        Task TogglePostLikeAsync(int postId, int userId);
+        Task TogglePostFavouriteAsync(int postId, int userId);
+        Task ToggleVisibilityAsync(int postId, int userId);
+        Task ReportPostAsync(int postId, int userId);
+    }
+}
---
+++ CircleApp.Data/Services/PostService.cs
@@ -0,0 +1,62 @@
+﻿
+using CircleApp.Data.Persistence.Entities;
+using CircleApp.Persistence;
+using Microsoft.AspNetCore.Http;
+
+namespace CircleApp.Data.Services
+{
+    public class PostService(CircleAppDbContext context) : IPostsService
+    {
+        private readonly CircleAppDbContext _context = context;
+
+        public Task AddPostCommentAsync(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreatePostAsync(Post post, IFormFile Image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePostAsync(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPostByIdAsync(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Post>> GetPostsForUserAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePostCommentAsync(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReportPostAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TogglePostFavouriteAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TogglePostLikeAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ToggleVisibilityAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
